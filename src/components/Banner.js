8c5dd895fd6f5bcab97d31f245b7041575a46185
Stackbit Code Editor <projects@stackbit.com>
2021-09-23 22:48:09 +0000

    Banner.js: changed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,8 +7,8 @@ import Icon from './Icon';
 
 export default class Banner extends React.Component {
     render() {
-        // const action = _.get(this.props, 'action');
-        // const url = _.get(action, 'url');
+        const action = _.get(this.props, 'action');
+        const url = _.get(action, 'url');
         // const newWindow = _.get(action, 'new_window');
         // const noFollow = _.get(action, 'no_follow');
         // const attrs = {};
